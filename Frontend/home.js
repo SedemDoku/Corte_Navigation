e3a4ca339a3ed0419aa49f9f3914d0b38e3ea53b
Gacucu <168026818+GKK2@users.noreply.github.com>
2025-11-14 22:51:37 +0000

    Delete Frontend/home.js

--- Frontend/home.js
+++
@@ -1,5 +0,0 @@
-// home.js
-document.getElementById("getStartedBtn").addEventListener("click", () => {
-  // Send user to dashboard (dashboard requires login)
-  window.location.href = "dashboard.html";
-});
